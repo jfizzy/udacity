12eb4e79b5ccd2452b5f4cef14bce13f3480ded9
jamzor <james@jmproj.com>
2016-05-26 14:53:33 -0600

    changes to forum
    made changes in the forum folder file to adapt for something we were
    testing in a quiz

--- Relational DB Projects/vagrant/forum/forumdb.py
+++ Relational DB Projects/vagrant/forum/forumdb.py
@@ -8,6 +8,8 @@ import time
 def GetAllPosts():
     DB = psycopg2.connect("dbname=forum")
     c = DB.cursor()
+    c.execute("UPDATE posts SET content = 'Cheese' WHERE content LIKE '%Spam, spam, spam, spam%Wonderful spam, glorious spam!%'")
+    c.execute("DELETE FROM posts WHERE content LIKE '%Cheese%'")
     c.execute("SELECT time,content FROM posts ORDER BY time DESC")
     posts = ({'content': str(row[1]), 'time': str(row[0])}
              for row in c.fetchall())
@@ -18,6 +20,7 @@ def GetAllPosts():
 def AddPost(content):
     DB = psycopg2.connect("dbname=forum")
     c = DB.cursor()
-    c.execute("INSERT INTO posts (content) VALUES ('%s')" % (content,))
+    data = (content,)
+    c.execute("INSERT INTO posts (content) VALUES (%s)", data) # getting the comma to replace the % was issue
     DB.commit()
     DB.close()
